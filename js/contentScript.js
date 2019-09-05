498be1bb11f6a806a16af7f48536607b4376b26b
itaymerchav <imerchav@first.media>
2019-09-05 12:16:00 +0300

    nice work

--- js/contentScript.js
+++ js/contentScript.js
@@ -1,29 +1,49 @@
-console.info('--------------------------------------');
-console.info('---- First Media Fucking Facebook ----');
-console.info('--------------------------------------');
-debugger;
+console.info('-------------------------------------------');
+console.info('---- First Media Extract Facebook Data ----');
+console.info('-------------------------------------------');
 
-// let myString = `
-// let myFn = function() { return 'fuck';};
-// window.XMLHttpRequest = myFn;
-// window.XDomainRequest = myFn;
-// let XMLHttpRequest = myFn;
-// let XDomainRequest = myFn;
-debugger;
-alert('')
-// `;
-// let header = document.getElementsByTagName('html');
-//
-// let script = document.createElement('script');
-// script.src = 'js/XMLHttpRequest.js';
-// //script.innerHTML = myString;
-//
-// header[0].appendChild(script);
+let html = document.getElementsByTagName('html')[0];
 
-chrome.webRequest.onBeforeRequest.addListener(
-    function(details) {
-        console.log(details);
-        return {cancel: false};
-    },
-    {urls: ["*://*/*"]},
-    ["what"]);
+let script = document.createElement('script');
+script.src = chrome.extension.getURL('js/injected.js');
+script.onload = function() {
+    this.remove();
+};
+html.appendChild(script);
+
+function startRecording() {
+    let script = document.createElement('script');
+    script.innerText = 'window._474_recording = true;';
+    script.innerText += 'document.body.style.border="5px solid red"';
+    script.onload = function() {
+        this.remove();
+    };
+    html.appendChild(script);
+}
+function endRecording() {
+    let script = document.createElement('script');
+    script.innerText = 'window._474_recording = false; download(window._474_dataArray);window._474_dataArray=[];';
+    script.innerText += 'document.body.style.border="none"';
+    script.onload = function() {
+        this.remove();
+    };
+    html.appendChild(script);
+}
+
+chrome.runtime.onMessage.addListener(
+    function(message, sender, sendResponse) {
+        switch(message.action) {
+            case "getState":
+                break;
+            case "START":
+                startRecording();
+                break;
+            case "END":
+                endRecording();
+                break;
+            default:
+                sendResponse(window._474_recording);
+        }
+        sendResponse(window._474_recording);
+    }
+);
